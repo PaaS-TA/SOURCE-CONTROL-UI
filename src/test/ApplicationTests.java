e645b9951499a28ad14ce8d3eb19c8fe1c0839f9
ijlee <214304>
2017-10-20 19:03:50 +0900

    test

---
+++ src/test/ApplicationTests.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTests {
+
+    public static void main(String[] args) {
+        // Tell server to look for registration.properties or registration.yml
+    }
+}
