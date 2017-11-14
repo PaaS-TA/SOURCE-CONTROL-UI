6a1f1d28d2847c16d6ee1aeea1393e532931b98c
ijlee <214304>
2017-11-14 17:23:31 +0900

    Service 병합
    Signed-off-by: ijlee <ㅛㅛㅓ214304>

--- src/main/java/com/paasta/scwui/common/util/RestClientUtil.java
+++ src/main/java/com/paasta/scwui/common/util/RestClientUtil.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
